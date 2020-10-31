54edcf2bca06297d5b70f212db8561d7fcec0a8e
Jason Prince <jasoncprince@gmail.com>
2020-10-31 06:44:15 -0600

    add a bunch of svgs

--- components/skills.js
+++ components/skills.js
@@ -1,38 +1,26 @@
+import HTML5 from "../svg/html5.svg";
+import Sass from "../svg/sass.svg";
+import JS from "../svg/javascript.svg";
+import Python from "../svg/python.svg";
+import CSS3 from "../svg/css3.svg";
+
 const skills = () => {
   return (
-    <section className="skills">
-      <div className="skills__icon">
-        <i className="skills__icon--html">HTML</i>
+    <section>
+      <div className="icon--margin icon__html--size">
+        <HTML5 />
       </div>
-      <div className="skills__icon">
-        <i className="skills__icon--css">CSS</i>
+      <div className="icon--margin icon__css--size">
+        <CSS3 />
       </div>
-      <div className="skills__icon">
-        <i className="skills__icon--sass">Sass</i>
+      <div className="icon--margin icon__sass--size">
+        <Sass />
       </div>
-      <div className="skills__icon">
-        <i className="skills__icon--styled-components">Styled Components</i>
+      <div className="icon--margin icon__js--size">
+        <JS />
       </div>
-      <div className="skills__icon">
-        <i className="skills__icon--javascript">Javascript</i>
-      </div>
-      <div className="skills__icon">
-        <i className="skills__icon--python">Python</i>
-      </div>
-      <div className="skills__icon">
-        <i className="skills__icon--react">React</i>
-      </div>
-      <div className="skills__icon">
-        <i className="skills__icon--nextjs">Next JS</i>
-      </div>
-      <div className="skills_icon">
-        <i className="skills__icon--github">Github</i>
-      </div>
-      <div className="skills_icon">
-        <i className="skills__icon--nodejs">Node JS</i>
-      </div>
-      <div className="skills_icon">
-        <i className="skills__icon--postgress">Postgress</i>
+      <div className="icon--margin icon__python--size">
+        <Python />
       </div>
     </section>
   );
