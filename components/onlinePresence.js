8f59ae37e57b15ef64dc8f661ee3b4b6ea0ad5af
Jason Prince <jasoncprince@gmail.com>
2020-10-30 19:04:14 -0600

    onlinePresence component created

---
+++ components/onlinePresence.js
@@ -0,0 +1,13 @@
+const onlinePresence = () => {
+  return (
+    <section className="contact">
+      <div className="contact__twiter">Twiter</div>
+      <div className="contact__github">github</div>
+      <div className="contact__linkedin">Linkedin</div>
+      <div className="contact__phone">Phone</div>
+      <div className="contact__email">Email</div>
+    </section>
+  );
+};
+
+export default onlinePresence;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Header from "../components/header";
+import OnlinePresence from "../components/onlinePresence";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <OnlinePresence />
+    </div>
+  );
 };
 
 export default Home;
