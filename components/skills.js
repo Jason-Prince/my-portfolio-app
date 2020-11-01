7c0be93a4da5a239072dda37b770ab7a878279a2
Jason Prince <jasoncprince@gmail.com>
2020-11-01 15:05:44 -0700

    added more svg icons

--- components/skills.js
+++ components/skills.js
@@ -1,27 +1,27 @@
-import HTML5 from "../svg/html5.svg";
-import Sass from "../svg/sass.svg";
-import JS from "../svg/javascript.svg";
+import Github from "../svg/github.svg";
+import MongoDB from "../svg/mongodb.svg";
+import Django from "../svg/django.svg";
 import Python from "../svg/python.svg";
-import CSS3 from "../svg/css3.svg";
+import Javascript from "../svg/javascript.svg";
+import Sass from "../svg/sass.svg";
+import CSS from "../svg/css3.svg";
+import HTML from "../svg/html5.svg";
+import NextJS from "../svg/nextjs.svg";
+import NodeJS from "../svg/nodejs.svg";
 
 const skills = () => {
   return (
-    <section>
-      <div className="icon--margin icon__html--size">
-        <HTML5 />
-      </div>
-      <div className="icon--margin icon__css--size">
-        <CSS3 />
-      </div>
-      <div className="icon--margin icon__sass--size">
-        <Sass />
-      </div>
-      <div className="icon--margin icon__js--size">
-        <JS />
-      </div>
-      <div className="icon--margin icon__python--size">
-        <Python />
-      </div>
+    <section className="section__icons">
+      <HTML />
+      <Django />
+      <Github />
+      <MongoDB />
+      <Python />
+      <Javascript />
+      <Sass />
+      <CSS />
+      <NextJS />
+      <NodeJS />
     </section>
   );
 };
